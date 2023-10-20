3b2e678443ac5edeb1d719f285e6b75e3fe42ef8
Mr Etim <etimvictordavid2@gmail.com>
2023-10-19 23:36:49 -0500

    conflict 1

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,8 @@ echo good job.
 Tested and accepted
 HEAD
 f503d99 (tested and approved by the client QA team)
+<<<<<<< HEAD
 f503d99 (tested and approved by the client QA team)
 ac806a3 (conflict resolved deploy.sh)
+=======
+>>>>>>> 71f07c4 (correction made to deplpy.sh)
