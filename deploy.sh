52d7551566d39a58415fbbe83fa127922b9e9bf6
Mr Etim <etimvictordavid2@gmail.com>
2023-10-19 23:33:54 -0500

    resolve conflict

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
 dev env set up
 deployment5 master
 deployment4 dev
+c806a3 (conflict resolved deploy.sh)
 deploy_Testing
 deploy to Testing evn
 echo good job. 
 Tested and accepted
+f503d99 (tested and approved by the client QA team)
+ac806a3 (conflict resolved deploy.sh)
